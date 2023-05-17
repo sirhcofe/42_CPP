8b163ae21df3c0f8ef4ee8519c222c93b2b1e3a4
Zer <livefatezz@gmail.com>
2023-05-17 17:16:13 +0800

    cpp03: add documentation

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,12 +6,31 @@
 /*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:58:45 by chenlee           #+#    #+#             */
-/*   Updated: 2023/05/17 16:59:39 by chenlee          ###   ########.fr       */
+/*   Updated: 2023/05/17 17:15:58 by chenlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
+/**
+ * In multiple inheritance, when a derived class inherits from multiple base
+ * classes that have a common base class, the derived class can end up with
+ * multiple copies of the common base class. This can lead to ambiguity and
+ * conflicts when accessing members of the common base class.
+ * 
+ * Virtual inheritance is a feature in C++ that allows a derived class to inherit
+ * a base class in such a way that only a single instance of the base class is
+ * shared among all the derived classes in a class hierarchy. This means that if
+ * a class hierarchy involves multiple inheritance paths to a common base class,
+ * using virtual inheritance ensures that there is no duplication of the base class.
+ * 
+ * This resolves the ambiguity and prevents duplication of the base class.
+ * 
+ * Note that using virtual inheritance comes with some runtime and memory
+ * overhead due to the need for additional bookkeeping to maintain the shared
+ * base class instance. Therefore, it should be used judiciously when necessary
+ * to resolve inheritance-related problems.
+*/
 int	main()
 {
 	DiamondTrap trap("Jason");
